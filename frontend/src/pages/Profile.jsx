b236333c0cd84782884673ac8047adedd2abf445
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-04-10 01:41:51 +0545

    update

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import uploadImg from "../assets/uploadimg.png";
 import { updateUser } from "../features/auth/authSlice"; // ✅ Correct import
+import NewsLetter from "../components/NewsLetter";
 
 const Profile = () => {
   const [profileImg, setProfileImg] = useState(null);
@@ -106,71 +107,85 @@ const Profile = () => {
   };
 
   return (
-    <div className="max-w-lg mx-auto p-6 bg-base-200 rounded-xl shadow-lg mt-10">
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <label className="block text-gray-400 mb-3">Profile Image</label>
-        <label htmlFor="image1" className="cursor-pointer overflow-hidden">
-          <img
-            src={profileImg ? URL.createObjectURL(profileImg) : uploadImg}
-            alt="profile_image"
-            className="object-cover w-30 h-30"
-          />
-          <input type="file" id="image1" hidden onChange={handleImageChange} />
-        </label>
-        {errors.avatar && (
-          <p className="text-xs text-red-500">{errors.avatar.message}</p>
-        )}
-
-        <div className="grid grid-cols-2 gap-4 mt-5">
-          <div>
-            <label className="block text-gray-400">First Name</label>
+    <>
+      <div className="max-w-lg mx-auto mb-20 p-6 bg-base-200 rounded-xl shadow-lg mt-10">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <label className="block text-gray-400 mb-3">Profile Image</label>
+          <label htmlFor="image1" className="cursor-pointer overflow-hidden">
+            <img
+              src={profileImg ? URL.createObjectURL(profileImg) : uploadImg}
+              alt="profile_image"
+              className="object-cover w-30 h-30"
+            />
             <input
-              type="text"
-              {...register("firstName", { required: "First name is required" })}
+              type="file"
+              id="image1"
+              hidden
+              onChange={handleImageChange}
+            />
+          </label>
+          {errors.avatar && (
+            <p className="text-xs text-red-500">{errors.avatar.message}</p>
+          )}
+
+          <div className="grid grid-cols-2 gap-4 mt-5">
+            <div>
+              <label className="block text-gray-400">First Name</label>
+              <input
+                type="text"
+                {...register("firstName", {
+                  required: "First name is required",
+                })}
+                className="input input-bordered w-full"
+              />
+              {errors.firstName && (
+                <p className="text-red-500 text-sm">
+                  {errors.firstName.message}
+                </p>
+              )}
+            </div>
+            <div>
+              <label className="block text-gray-400">Last Name</label>
+              <input
+                type="text"
+                {...register("lastName", { required: "Last name is required" })}
+                className="input input-bordered w-full"
+              />
+              {errors.lastName && (
+                <p className="text-red-500 text-sm">
+                  {errors.lastName.message}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div>
+            <label className="block text-gray-400">Age</label>
+            <input
+              type="number"
+              {...register("age", { required: "Age is required" })}
               className="input input-bordered w-full"
             />
-            {errors.firstName && (
-              <p className="text-red-500 text-sm">{errors.firstName.message}</p>
+            {errors.age && (
+              <p className="text-red-500 text-sm">{errors.age.message}</p>
             )}
           </div>
-          <div>
-            <label className="block text-gray-400">Last Name</label>
-            <input
-              type="text"
-              {...register("lastName", { required: "Last name is required" })}
-              className="input input-bordered w-full"
-            />
-            {errors.lastName && (
-              <p className="text-red-500 text-sm">{errors.lastName.message}</p>
+
+          <button
+            disabled={updateMutation.isPending}
+            type="submit"
+            className="btn btn-primary w-full mt-4"
+          >
+            {updateMutation.isPending ? (
+              <Loader2 className="h-4 w-4 mr-1 animate-spin" />
+            ) : (
+              "Update Profile"
             )}
-          </div>
-        </div>
-
-        <div>
-          <label className="block text-gray-400">Age</label>
-          <input
-            type="number"
-            {...register("age", { required: "Age is required" })}
-            className="input input-bordered w-full"
-          />
-          {errors.age && (
-            <p className="text-red-500 text-sm">{errors.age.message}</p>
-          )}
-        </div>
-
-        <button
-          disabled={updateMutation.isPending}
-          type="submit"
-          className="btn btn-primary w-full mt-4"
-        >
-          {updateMutation.isPending ? (
-            <Loader2 className="h-4 w-4 mr-1 animate-spin" />
-          ) : (
-            "Update Profile"
-          )}
-        </button>
-      </form>
-    </div>
+          </button>
+        </form>
+      </div>
+      <NewsLetter />
+    </>
   );
 };
 
